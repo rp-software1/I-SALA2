433591184d194f04c6c6e62f0148d3d510604360
carlos <carlos.campusano15042007@gmail.com>
2026-03-24 15:57:26 -0500

    platocard,home y hot reload funcionando

--- dia2/restaurante_cayda/src/pages/home.jsx
+++ dia2/restaurante_cayda/src/pages/home.jsx
@@ -1,8 +1,16 @@
+import { platosMock } from "../data/platos.mock.js";
+import PlatoCard from "../components/PlatoCard.jsx";
+
 function Home() {
     return (
         <>
             <h1>Carta del Restaurante</h1>
             <p>Aqui van los platos</p>
+            <ul>
+                {platosMock.map(plato => (
+                    <PlatoCard key={plato.id} plato={plato} />
+                ))}
+            </ul>
         </>
     )
 }
