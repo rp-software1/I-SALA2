ce5b02623235e5001ef535cf0ea7c245fe16e93f
carlos <carlos.campusano15042007@gmail.com>
2026-04-07 13:03:55 -0500

    arreglando

--- dia2/restaurante_cayda/src/data/platos.mock.js
+++ dia2/restaurante_cayda/src/data/platos.mock.js
@@ -1,8 +1,8 @@
 export const platosMock = [
     { id: 1, nombre: "estofado", categoria: "segundos", precio: 10, stock: 3, disponible: true },
     { id: 2, nombre: "Aji de gallina", categoria: "segundos", precio: 12, stock: 5, disponible: true },
-    { id: 3, nombre: "Sopa de pollo", categoria: "entradas", precio: 15, stock: 0, disponible: false },
-    { id: 4, nombre: "Pollo a la brasa", categoria: "segundos", precio: 20, stock: 4, disponible: true },
+    { id: 3, nombre: "lomo saltado", categoria: "segundos", precio: 15, stock: 0, disponible: false },
+    { id: 4, nombre: "Arroz con pollo", categoria: "segundos", precio: 20, stock: 4, disponible: true },
     { id: 5, nombre: "inka-kola", categoria: "bebidas", precio: 3, stock: 8, disponible: true },
 ];
 
