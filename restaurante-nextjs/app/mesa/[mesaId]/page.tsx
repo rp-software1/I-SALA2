1dc651820b2dc3897c9a00be87ec921406b669b7
carlos <carlos.campusano15042007@gmail.com>
2026-05-07 12:13:45 -0500

    corriguiendo

--- restaurante-nextjs/app/mesa/[mesaId]/page.tsx
+++ restaurante-nextjs/app/mesa/[mesaId]/page.tsx
@@ -1,15 +1,29 @@
 interface MesaDetailProps {
-    params: {
-        mesaId: string;  // siempre string — Next.js no lo convierte a número
-    };
+    params: Promise<{
+        mesaId: string;
+    }>;
 }
 
-export default function MesaDetailPage({ params }: MesaDetailProps) {
+export default async function MesaDetailPage({ params }: MesaDetailProps) {
+
+    const { mesaId } = await params;
+
     return (
         <div>
-            <h1 className="text-2xl font-bold mb-4">Detalle de Mesa</h1>
-            <p>ID recibido: <span className="font-mono bg-gray-100 px-2 py-1 rounded">{params.mesaId}</span></p>
-            <p className="text-gray-500 mt-4">Aquí irá la información de la mesa — conectar en Día 4</p>
+            <h1 className="text-2xl font-bold mb-4">
+                Detalle de Mesa
+            </h1>
+
+            <p>
+                ID recibido:
+                <span className="font-mono bg-gray-100 px-2 py-1 rounded">
+                    {mesaId}
+                </span>
+            </p>
+
+            <p className="text-gray-500 mt-4">
+                Aquí irá la información de la mesa — conectar en Día 4
+            </p>
         </div>
     );
 }
